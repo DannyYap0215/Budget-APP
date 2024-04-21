afb1651f3ae1c4725475d175071ceb63c0e8cc0b
DannyYap0215 <dannychiyi@gmail.com>
2024-04-21 20:37:39 +0800

    switched whole program to customtkinter;fixed bug where value cant be added to optionmenu

--- edit_budget.py
+++ edit_budget.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from customtkinter import *
 import set_income
 import set_categories
 
@@ -10,14 +10,14 @@ def open_set_categories_window():
 
 def open_edit_budget_window():
     
-    edit_budget_window = tk.Toplevel()
+    edit_budget_window = CTkToplevel()
     edit_budget_window.title("Edit Budget")
     edit_budget_window.geometry("400x300")
 
-    set_income_button = tk.Button(edit_budget_window, text="Set Income",command=open_set_income_window)
+    set_income_button = CTkButton(edit_budget_window, text="Set Income",command=open_set_income_window)
     set_income_button.grid(row=0, column=0, padx=10, pady=5, sticky="w")
     
-    set_categories_button = tk.Button(edit_budget_window, text="Set Categories",command=open_set_categories_window)
+    set_categories_button = CTkButton(edit_budget_window, text="Set Categories",command=open_set_categories_window)
     set_categories_button.grid(row=1, column=0, padx=10, pady=5, sticky="w")
     
 
