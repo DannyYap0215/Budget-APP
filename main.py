b54b48a0324734d734e263531014a824cfb0215b
MeiTingTham <meiting0120@gmail.com>
2024-04-28 00:55:25 +0800

    nothing

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ def open_update_expenses_window():
 
 def open_insight_window():
     insight.open_insight_window()
+
+def open_expenses_history_window():
+    expenses_history.open_expenses_history_window()
     
 insight_button = CTkButton(root, text="Insight", height= 10,width=20, command=open_insight_window)
 insight_button.grid(row=0, column=1, padx=10, pady=5, sticky="w",)
@@ -31,6 +34,8 @@ edit_budget_button.grid(row=1, column=1, padx=10, pady=5,sticky="w")
 update_expenses_button = CTkButton(root, text="Update Expenses",height= 10,width=20, command=open_update_expenses_window)
 update_expenses_button.grid(row=2, column=1, padx=10, pady=5, sticky="w")
 
+expenses_history_button = CTkButton(root, text="Expenses History",height= 10,width=20, command=open_expenses_history_window)
+expenses_history_button.grid(row=3, column=1, padx=10, pady=5, sticky="w")
 
 root.bind("<F11>", toggle_fullscreen)
 
