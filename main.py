cd4acc601b4a3503a7e5e42f685dcc269958c275
MeiTingTham <meiting0120@gmail.com>
2024-05-27 03:28:27 +0800

    Connect update_expenses to main dashboard

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import insight
 
 
 root = CTk()
-root.title("Budget!")
+root.title("Budget Dashboard")
 screen_width = 1920
 screen_height = 1080
 root.geometry(f"{screen_width}x{screen_height}")
@@ -31,25 +31,46 @@ def indicator(label):
     hide_indicator()
     label.configure(fg_color="#8885B6", bg_color="#535085")
 
+def open_insight_window():
+    insight.open_insight_window(dashbard_right_frame)
+
+def open_edit_budget_window():
+    edit_budget.open_edit_budget_window(dashbard_right_frame)
+
+def open_update_expenses_window():
+    update_expenses.open_update_expenses_window(dashbard_right_frame)
+
+def open_expenses_history_window():
+    expenses_history.open_expenses_history_window(dashbard_right_frame)
+
 dashboard_left_frame = CTkFrame(master=root, width=420, height=1080, corner_radius=10, fg_color="#535085")
 dashboard_left_frame.place(relx=0.0, rely=0.5, anchor="w")
 
-insight_button = CTkButton(root, image=CTkImage(insight_icon), text="Insight", font=CTkFont("font/Poppins-Bold.ttf",35), corner_radius=10, fg_color="#8885B6", bg_color="#535085", hover_color="#2B2A45", text_color="#FFFFFF", command=lambda:indicator(insight_indicator))
+insight_button = CTkButton(root, image=CTkImage(insight_icon), text="Insight", font=CTkFont("font/Poppins-Bold.ttf",35), 
+                           corner_radius=10, fg_color="#8885B6", bg_color="#535085", hover_color="#2B2A45", text_color="#FFFFFF", 
+                           command=lambda:(indicator(insight_indicator), open_insight_window))
 insight_button.place(relx=0.03, rely=0.3, anchor="w")
 insight_indicator = CTkLabel(root, text="", width=8, height=45, bg_color="#535085")
 insight_indicator.place(relx=0.02, rely=0.3, anchor="w")
 
-edit_budget_button = CTkButton(root, image=CTkImage(edit_budget_icon), text="Edit Budget", font=CTkFont("font/Poppins-Bold.ttf",35), corner_radius=10, fg_color="#8885B6", bg_color="#535085", hover_color="#2B2A45", text_color="#FFFFFF", command=lambda:indicator(edit_budget_indicator))
+edit_budget_button = CTkButton(root, image=CTkImage(edit_budget_icon), text="Edit Budget", font=CTkFont("font/Poppins-Bold.ttf",35), 
+                               corner_radius=10, fg_color="#8885B6", bg_color="#535085", hover_color="#2B2A45", text_color="#FFFFFF", 
+                               command=lambda:(indicator(edit_budget_indicator), open_edit_budget_window))
 edit_budget_button.place(relx=0.03, rely=0.4, anchor="w")
 edit_budget_indicator = CTkLabel(root, text="", width=8, height=45, bg_color="#535085")
 edit_budget_indicator.place(relx=0.02, rely=0.4, anchor="w")
 
-update_expenses_button = CTkButton(root, image=CTkImage(update_expenses_icon), text="Update Expenses", font=CTkFont("font/Poppins-Bold.ttf",35), corner_radius=10, fg_color="#8885B6", bg_color="#535085", hover_color="#2B2A45", text_color="#FFFFFF", command=lambda:indicator(update_expenses_indicator))
+update_expenses_button = CTkButton(root, image=CTkImage(update_expenses_icon), text="Update Expenses", font=CTkFont("font/Poppins-Bold.ttf",35), 
+                                   corner_radius=10, fg_color="#8885B6", bg_color="#535085", hover_color="#2B2A45", text_color="#FFFFFF", 
+                                   command=lambda:(indicator(update_expenses_indicator), open_expenses_history_window))
 update_expenses_button.place(relx=0.03, rely=0.5, anchor="w")
 update_expenses_indicator = CTkLabel(root, text="", width=8, height=45, bg_color="#535085")
 update_expenses_indicator.place(relx=0.02, rely=0.5, anchor="w")
 
-expenses_history_button = CTkButton(root, image=CTkImage(expenses_history_icon), text="Expenses History", font=CTkFont("font/Poppins-Bold.ttf",35), corner_radius=10, fg_color="#8885B6", bg_color="#535085", hover_color="#2B2A45", text_color="#FFFFFF", command=lambda:indicator(expenses_history_indicator))
+expenses_history_button = CTkButton(root, image=CTkImage(expenses_history_icon), text="Expenses History", 
+                                    font=CTkFont("font/Poppins-Bold.ttf",35), corner_radius=10, fg_color="#8885B6", bg_color="#535085", 
+                                    hover_color="#2B2A45", text_color="#FFFFFF", 
+                                    command=lambda:(indicator(expenses_history_indicator), open_expenses_history_window))
 expenses_history_button.place(relx=0.03, rely=0.6, anchor="w")
 expenses_history_indicator = CTkLabel(root, text="", width=8, height=45, bg_color="#535085")
 expenses_history_indicator.place(relx=0.02, rely=0.6, anchor="w")
--- update_expenses.py
+++ update_expenses.py
@@ -18,9 +18,9 @@ import database_test as db
 #     "Gift",
 # ]
 
-expenses_data = []
-
-def open_update_expenses_window():
+def open_update_expenses_window(update_expenses_frame):
+    for widget in update_expenses_frame.winfo_children():
+        widget.destroy()
       
     categories = db.update_categories_list()  
       
@@ -31,74 +31,66 @@ def open_update_expenses_window():
     category_icon = Image.open("icon/category_icon.png")
     note_icon = Image.open("icon/note_icon.png")
 
-    update_expenses_window = CTkToplevel()
-    update_expenses_window.title("Update Expenses")
-    update_expenses_window.geometry("900x400")
-    update_expenses_window.wm_attributes("-topmost",True)
-
-    expenses_frame = CTkFrame(master=update_expenses_window, width=700, height=600,border_width=5, border_color="#3F3D65", corner_radius=15)
-    expenses_frame.grid(row=10, column=0, padx=20, pady=5)
-
     #Label for Update Expenses
-    update_expenses_title_label = CTkLabel(update_expenses_window, text="Update Expenses", font=CTkFont("font/Poppins-Bold.ttf",50,"bold") , text_color="#6965A3")
+    update_expenses_title_label = CTkLabel(update_expenses_frame, text="Update Expenses", font=CTkFont("font/Poppins-Bold.ttf",50,"bold") , text_color="#6965A3")
     update_expenses_title_label.grid(row=1, column=0, padx=20, pady=5, sticky="w")
 
     #Label for date
-    date_picker_label = CTkLabel(update_expenses_window, text="Date :", font=CTkFont("font/Poppins-Bold.ttf",30))
-    date_picker_label.grid(row=3, column=0, padx=50, pady=5, sticky="w", in_=expenses_frame)
+    date_picker_label = CTkLabel(update_expenses_frame, text="Date :", font=CTkFont("font/Poppins-Bold.ttf",30))
+    date_picker_label.grid(row=3, column=0, padx=50, pady=5, sticky="w")
 
-    date_icon_label = CTkLabel(update_expenses_window, text="",image= CTkImage(calendar_icon) )
-    date_icon_label.grid(row=3, column=0, padx=20, pady=5, sticky="w", in_=expenses_frame)
+    date_icon_label = CTkLabel(update_expenses_frame, text="",image= CTkImage(calendar_icon) )
+    date_icon_label.grid(row=3, column=0, padx=20, pady=5, sticky="w")
 
     def update_date_format(event):
         date_entry.set_date(date_entry.get_date().strftime("%d-%m-%Y"))
         
     #DateEntry widget for selecting date
-    date_entry = DateEntry(update_expenses_window, width=14, borderwidth=2, relief="groove", selectmode="day", date_pattern="dd-mm-yyyy", font=("Helvetica", 12))
-    date_entry.grid(row=3, column=1, padx=10, pady=5, sticky="w", in_=expenses_frame)
+    date_entry = DateEntry(update_expenses_frame, width=14, borderwidth=2, relief="groove", selectmode="day", date_pattern="dd-mm-yyyy", font=("Helvetica", 12))
+    date_entry.grid(row=3, column=1, padx=10, pady=5, sticky="w")
     date_entry.bind("<<DateEntrySelected>>", update_date_format)
 
     #Label for expenses amount
-    expenses_amount_label = CTkLabel(update_expenses_window, text="Expenses : ", font=CTkFont("font/Poppins-Bold.ttf",30))
-    expenses_amount_label.grid(row=6, column=0, padx=50, pady=5, sticky="w", in_=expenses_frame)
+    expenses_amount_label = CTkLabel(update_expenses_frame, text="Expenses : ", font=CTkFont("font/Poppins-Bold.ttf",30))
+    expenses_amount_label.grid(row=6, column=0, padx=50, pady=5, sticky="w")
 
-    expenses_icon_label = CTkLabel(update_expenses_window, text="",image= CTkImage(expenses_icon) )
-    expenses_icon_label.grid(row=6, column=0, padx=20, pady=5, sticky="w", in_=expenses_frame) 
+    expenses_icon_label = CTkLabel(update_expenses_frame, text="",image= CTkImage(expenses_icon) )
+    expenses_icon_label.grid(row=6, column=0, padx=20, pady=5, sticky="w") 
 
     def amount_entry_font_change(event):
         text = expenses_amount_entry.get
         expenses_amount_entry.configure(font=CTkFont("font/Poppins.ttf",26))
 
     #Entry for expenses amount
-    expenses_amount_entry = CTkEntry(update_expenses_window, width=150, height=34)
-    expenses_amount_entry.grid(row=6, column=1, padx=10, pady=5, sticky="w", in_=expenses_frame)
+    expenses_amount_entry = CTkEntry(update_expenses_frame, width=150, height=34)
+    expenses_amount_entry.grid(row=6, column=1, padx=10, pady=5, sticky="w")
     expenses_amount_entry.bind("<KeyRelease>", amount_entry_font_change)
 
     #Label for expenses category
-    expenses_categories_label = CTkLabel(update_expenses_window, text="Category :", font=CTkFont("font/Poppins-Bold.ttf",30))
-    expenses_categories_label.grid(row=9, column=0, padx=50, pady=5, sticky="w", in_=expenses_frame)
+    expenses_categories_label = CTkLabel(update_expenses_frame, text="Category :", font=CTkFont("font/Poppins-Bold.ttf",30))
+    expenses_categories_label.grid(row=9, column=0, padx=50, pady=5, sticky="w")
 
-    category_icon_label = CTkLabel(update_expenses_window, text="",image= CTkImage(category_icon) )
-    category_icon_label.grid(row=9, column=0, padx=20, pady=5, sticky="w", in_=expenses_frame) 
+    category_icon_label = CTkLabel(update_expenses_frame, text="",image= CTkImage(category_icon) )
+    category_icon_label.grid(row=9, column=0, padx=20, pady=5, sticky="w") 
 
     #dropdown expenses category menu
-    expenses_categories_menu = CTkOptionMenu(update_expenses_window, values=categories, width=150, height=34, fg_color="#6965A3")
-    expenses_categories_menu.grid(row=9, column=1, padx=10, pady=5, sticky="w", in_=expenses_frame)
+    expenses_categories_menu = CTkOptionMenu(update_expenses_frame, values=categories, width=150, height=34, fg_color="#6965A3")
+    expenses_categories_menu.grid(row=9, column=1, padx=10, pady=5, sticky="w")
 
     #Label for note
-    expenses_note_label = CTkLabel(update_expenses_window, text="Note :", font=CTkFont("font/Poppins-Bold.ttf",30))
-    expenses_note_label.grid(row=12, column=0, padx=50, pady=5, sticky="w", in_=expenses_frame)
+    expenses_note_label = CTkLabel(update_expenses_frame, text="Note :", font=CTkFont("font/Poppins-Bold.ttf",30))
+    expenses_note_label.grid(row=12, column=0, padx=50, pady=5, sticky="w")
 
-    note_icon_label = CTkLabel(update_expenses_window, text="",image= CTkImage(note_icon) )
-    note_icon_label.grid(row=12, column=0, padx=20, pady=5, sticky="w", in_=expenses_frame) 
+    note_icon_label = CTkLabel(update_expenses_frame, text="",image= CTkImage(note_icon) )
+    note_icon_label.grid(row=12, column=0, padx=20, pady=5, sticky="w") 
 
     def note_entry_font_change(event):
         text = expenses_note_entry.get
         expenses_note_entry.configure(font=CTkFont("font/Poppins.ttf",26))
 
     #Entry for note
-    expenses_note_entry = CTkEntry(update_expenses_window, width=150, height=34)
-    expenses_note_entry.grid(row=12, column=1, padx=10, pady=5, sticky="w", in_=expenses_frame)
+    expenses_note_entry = CTkEntry(update_expenses_frame, width=150, height=34)
+    expenses_note_entry.grid(row=12, column=1, padx=10, pady=5, sticky="w")
     expenses_note_entry.bind("<KeyRelease>", note_entry_font_change)
 
     def save_expenses():
@@ -106,16 +98,16 @@ def open_update_expenses_window():
         expenses_amount = expenses_amount_entry.get()
         expenses_categories = expenses_categories_menu.get()
         expenses_note = expenses_note_entry.get()
-        expenses_data.append((expenses_date, expenses_amount, expenses_categories, expenses_note))
+        # expenses_data.append((expenses_date, expenses_amount, expenses_categories, expenses_note))
         db.insert_expenses_to_table(expenses_date,expenses_amount,expenses_categories,expenses_note)
         # update_expenses_window.destroy()
 
 
     #Button to save expenses
-    save_expenses_button = CTkButton(update_expenses_window, text="Save Expenses", font=CTkFont("font/Poppins-Bold.ttf",25), fg_color="#6965A3", hover_color="#8885B6", image=CTkImage(save_icon), command=save_expenses)
-    save_expenses_button.grid(row=18, column=0, padx=10, pady=5, sticky="w", in_=expenses_frame)
+    save_expenses_button = CTkButton(update_expenses_frame, text="Save Expenses", font=CTkFont("font/Poppins-Bold.ttf",25), fg_color="#6965A3", hover_color="#8885B6", image=CTkImage(save_icon), command=save_expenses)
+    save_expenses_button.grid(row=18, column=0, padx=10, pady=5, sticky="w")
 
-    clock_frame = CTkFrame(master=update_expenses_window, width=700, height=600,border_width=5, border_color="#3F3D65", corner_radius=15)
+    clock_frame = CTkFrame(master=update_expenses_frame, width=700, height=600,border_width=5, border_color="#3F3D65", corner_radius=15)
     clock_frame.grid(row=10, column=4, padx=20, pady=5, sticky="n")
 
     def clock_time ():
@@ -123,6 +115,6 @@ def open_update_expenses_window():
         clock_label.configure(text=clock_string)
         clock_label.after(1000, clock_time)
 
-    clock_label = CTkLabel(update_expenses_window, font=CTkFont("Jersey.ttf", 40, "bold"))
+    clock_label = CTkLabel(update_expenses_frame, font=CTkFont("Jersey.ttf", 40, "bold"))
     clock_label.grid(row=0, column=0, padx=60, pady=5, sticky="e", in_=clock_frame)
     clock_time()
